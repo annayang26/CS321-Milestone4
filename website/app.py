f7063e3e367ffaa72564da379b802a2f7dcb5868
szaccaro <sazacc24@colby.edu>
2022-10-25 11:25:04 -0400

    index.html update

--- website/app.py
+++ website/app.py
@@ -8,15 +8,20 @@
 
 # app.run(host='0.0.0.0', port=81)
 
-from flask import Flask,render_template
-  
+from flask import Flask, render_template
+
+
   
 app = Flask(__name__)
   
 @app.route("/")
 def home():
     return render_template("index.html")
-  
+
+@app.route("/reportpage")
+def reportpage():
+    return render_template("reportpage.html")
+
   
 if __name__ == "__main__":
     app.run(debug=True)
