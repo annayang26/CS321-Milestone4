6e91c7efcc7a22a83827a8d16f21abd614f30c0b
adaobi5 <anebuw24@colby.edu>
2022-11-20 22:43:00 -0500

    edits to coach and athlete dashboard

--- website/views.py
+++ website/views.py
@@ -86,17 +86,17 @@ def athlete_breakdown():
 @views.route('/sleep')
 @login_required
 def sleep_breakdown():
-    if current_user.access == 0 or current_user.access == 1:
+    if current_user.access >= 0:
         return render_template('sleep.html', user=current_user)
 
 @views.route('/recovery')
 @login_required
 def recovery_breakdown():
-    if current_user.access == 0 or current_user.access == 1:
+    if current_user.access >= 0:
         return render_template('recovery.html', user=current_user)
 
 @views.route('/calories')
 @login_required
 def calories_breakdown():
-    if current_user.access == 0 or current_user.access == 1:
+    if current_user.access >= 0:
         return render_template('calories.html', user=current_user)
