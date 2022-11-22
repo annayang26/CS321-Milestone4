c2356cfb9e1e35cc5ee222b62a9ff8801913da2b
linncao53 <lcaong24@colby.edu>
2022-11-21 23:37:43 -0500

    fix models and test_models

--- website/models.py
+++ website/models.py
@@ -40,10 +40,12 @@ class User(db.Model, UserMixin):
     # def get_access(self):
     #     return self.access
 
-    def __init__(self, email, password, first_name, last_name, access):
+    def __init__(self, email, password, first_name, last_name, access, branch, team):
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
         self.access = access
+        self.branch = branch
+        self.team = team
 
